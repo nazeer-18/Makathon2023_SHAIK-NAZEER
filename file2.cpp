e05db7224cf4115f1cf311f9a711eb80fa6435d1
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-02-27 14:23:45 +0530

    added comments

--- file2.cpp
+++ file2.cpp
@@ -1,3 +1,7 @@
+/*
+21161 shaik nazeer
+*/
+
 #include<bits/stdc++.h>
 #define ll long long
 #define loop(i,n) for(int i = 0; i < n; i++)
@@ -10,7 +14,19 @@ int main()
     while(t--){
         int n;
         cin>>n;
-
+         /*
+        for a given n all prime subsequences should contain white beads greater than black beads so 
+        say for n = 3 
+        possible cases are 
+            white   black
+            3       0
+            2       1
+        now these beads can be arranged in any ways so we can use combinations here
+            which gives us 3c3 + 3c2
+            which is half of binomail coefficient of 2^n
+            so the desired output is least integer function of pow of n to the base 2 divided ny 2
+            so the output is : int((pow(2,n)+1)/2) 
+        */
         cout<<int((pow(2,n)+1)/2)<<endl;
     }
     return 0;
